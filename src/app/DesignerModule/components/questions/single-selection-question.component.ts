3cdd2a9747108b64e52894e626ce0bf66c1c3909
zalkovalsky <kristofer90@gmail.com>
2017-08-18 19:58:51 +0200

    Refactoring for BaseElement

--- src/app/DesignerModule/components/questions/single-selection-question.component.ts
+++ src/app/DesignerModule/components/questions/single-selection-question.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { DynamicField } from './../../models/dynamic-field.interface';
-import { BaseElement,Option } from './../../models/base-element';
+import { BaseElement, Option } from './../../models/base-element';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -11,24 +11,13 @@ export class SingleSelectionQuestionComponent implements DynamicField {
     element: BaseElement;
     formGroup: FormGroup;
 
-    trackByIndex(index: number, obj: Option) {
-        return obj.index;
-    }
     pushOption(): void {
-
-        console.trace('traced');
-
-        if (!this.element.options) {
-            this.element.options = new Array<Option>();
-        }
-
-        let option = new Option(this.element.options.length + 1, 'Please edit me');
-        
-        this.element.options.push(option);
+        this.element.pushOption('Please edit me');
     }
 
-    popOption(option: string): void {
-        // TODO: make strongly-typed
-        // this.element.options = this.element.options.filter((o) => o === option);
+    popOption(option: Option): void {
+        let index = this.element.options.indexOf(option);
+
+        this.element.options.splice(index, 1);
     }
 }
--- src/app/DesignerModule/models/base-element.ts
+++ src/app/DesignerModule/models/base-element.ts
@@ -4,7 +4,6 @@ import { ValidatorFn } from '@angular/forms';
 
 export class Option {
     constructor(public index: number, public value: string) { }
-
 }
 
 export class BaseElement implements Movable {
@@ -21,11 +20,32 @@ export class BaseElement implements Movable {
     isMoving: boolean;
 
     additionalText?: string;
-    options?: Option[];
+    options?: Option[] = [];
     placeholderText?: string;
 
     constructor(init?: Partial<BaseElement>) {
         Object.assign(this, init);
+    }
+
+    pushOption(value: string) {
+        if (!value) {
+            throw new Error('New option value has to defined');
+        }
+
+        let ids: number[] = this.options.map(x => {
+            return x.index
+        });
+        let nextId = Math.max(...ids) + 1;
+
+        this.options.push(new Option(nextId, value));
+    }
+
+    removeOption(option: Option) {
+        let index = this.options.indexOf(option);
+
+        if (index !== -1) {
+            this.options.splice(index, 1);
+        }
     }
 }
 
