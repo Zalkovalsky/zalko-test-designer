ac8a41c8f05272e12e082829585a5d5af18f2eb2
Zalkovalsky <kristofer90@gmail.com>
2017-10-29 14:33:36 +0100

    Fixed an issue with zombie processes in the prod mode, modified package script

--- src/electron-app.js
+++ src/electron-app.js
@@ -1,9 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var electron_1 = require("electron");
+var logger = require('electron-log');
 var mainWindow = null;
 electron_1.app.on('ready', function () {
+    logger.info('[app.ready] Creating new BrowserWindow');
     mainWindow = new electron_1.BrowserWindow({ width: 800, height: 800 });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.on('closed', function () {
+        logger.info('[mainWindow.closed] Removing window reference');
+        mainWindow = null;
+    });
+});
+electron_1.app.on('window-all-closed', function () {
+    logger.info('[app.window-all-closed] All windows closed, killing app');
+    electron_1.app.quit();
 });
 //# sourceMappingURL=electron-app.js.map
--- src/electron-app.ts
+++ src/electron-app.ts
@@ -1,8 +1,23 @@
 import {app, BrowserWindow} from 'electron';
 
-let mainWindow = null;
+const logger = require('electron-log');
+
+let mainWindow: any = null;
 
 app.on('ready', () => {
+
+    logger.info('[app.ready] Creating new BrowserWindow');
     mainWindow = new BrowserWindow({ width: 800, height: 800});
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', () => {
+        logger.info('[mainWindow.closed] Removing window reference');
+        mainWindow = null;
+    });
 });
+
+app.on('window-all-closed', () => {
+    logger.info('[app.window-all-closed] All windows closed, killing app');
+    app.quit();
+});
+
